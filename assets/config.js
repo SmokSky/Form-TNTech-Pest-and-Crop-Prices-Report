dc0a2c3173353b88775b0f2f243e98ecd0a406f1
TNDev <tndev1991@gmail.com>
2026-04-13 15:08:06 +0700

    Update SUBMIT_TOKEN in config.js for security compliance

--- assets/config.js
+++ assets/config.js
@@ -4,6 +4,6 @@
  */
 window.PEST_REPORT_CONFIG = {
   WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyAz5sm_KG1aiNKjpoWRwtmNzmvzPgi01FTC5dnxHKm682cLA0XRbmfTQr4AItcIII/exec',
-  SUBMIT_TOKEN: 'AKfycbyAz5sm_KG1aiNKjpoWRwtmNzmvzPgi01FTC5dnxHKm682cLA0XRbmfTQr4AItcIII',
+  SUBMIT_TOKEN: 'D1jk1pIi9EJfCyB6awf07kf20YKYAtg9',
   GOOGLE_CLIENT_ID: '132295089348-1u8mqnovhiudl18mv1tm5lvfq6rhe45c.apps.googleusercontent.com'
 };
